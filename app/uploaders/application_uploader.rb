e4622f4c777a6e2b2af23c25b8d43bd9b6f03abe
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-04-22 05:12:38 +0400

    Add optional uuid to files paths

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -6,6 +6,7 @@ class ApplicationUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "system/uploads/#{model.class.model_name.to_s.underscore}/#{mounted_as}/#{model.id}"
+    directory_id = (model.reload && model.try(:uuid)) || model.id
+    "system/uploads/#{model.class.model_name.to_s.underscore}/#{mounted_as}/#{directory_id}"
   end
 end
