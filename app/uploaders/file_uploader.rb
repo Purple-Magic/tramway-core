238dcb8876a2c476c20c753df6aa6c4d091a3cd6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-04-26 23:57:23 +0400

    Add whitelist extensions

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FileUploader < ApplicationUploader
-  def extension_white_list
-    %w[pdf doc docx xls csv xlsx jpg svg png jpeg gif]
+  def extension_whitelist
+    model.class.file_extensions || %w[pdf doc docx xls csv xlsx jpg svg png jpeg gif]
   end
 end
