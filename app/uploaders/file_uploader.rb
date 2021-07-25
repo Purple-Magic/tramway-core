15c735aa42d9b212720165adf11178f0ee4adaef
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-07-25 09:52:29 +0400

    Fix file extenstions

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class FileUploader < ApplicationUploader
-  def extension_whitelist
-    model.class.file_extensions || %w[pdf doc docx xls csv xlsx jpg svg png jpeg gif]
-  end
 end
